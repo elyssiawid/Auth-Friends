e5cb63206574ef121a6db1f73cac5b44938d45bc
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-08-20 16:18:51 -0700

    reached MVP

---
+++ friends/src/components/AxiosWithAuth.js
@@ -0,0 +1,10 @@
+import Axios from "axios";
+
+const AxiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+  return Axios.create({
+    headers: { authorization: token }
+  });
+};
+
+export default AxiosWithAuth;
